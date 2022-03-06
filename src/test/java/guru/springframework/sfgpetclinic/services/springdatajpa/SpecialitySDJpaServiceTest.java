090ad9494e9e787bf0c8464d3f18dfe7fd183357
Anant Tripathi <ianantktripathi@gmail.com>
2022-03-06 11:42:26 +0530

    Behaviour Driven development:
    Given-When-Then Testing methods.

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -14,6 +14,8 @@ import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,43 +27,69 @@ class SpecialitySDJpaServiceTest {
     @InjectMocks
     SpecialitySDJpaService service;
 
+
     @Test
-    void findByIdTest(){
+    void findByIdBDDTest() {
+        //Given::::
         Speciality speciality=new Speciality();
-        when(specialtyRepository.findById(1l)).thenReturn(Optional.of(speciality));
+        given(specialtyRepository.findById(1l)).willReturn(Optional.of(speciality));
+        //When:::
         Speciality foundSpeciality=service.findById(1l);
+        //Then:::
         assertThat(foundSpeciality).isNotNull();
-        verify(specialtyRepository,times(1)).findById(anyLong());
+        //verify(specialtyRepository).findById(anyLong());
+        then(specialtyRepository).should().findById(anyLong());
+        then(specialtyRepository).shouldHaveNoMoreInteractions();
     }
 
     @Test
     void delete() {
+        //given-- NONE
+        //When--
         specialtyRepository.delete(new Speciality());
-        verify(specialtyRepository).delete(any(Speciality.class));
+        //then--
+        //verify(specialtyRepository).delete(any(Speciality.class));
+        then(specialtyRepository).should().delete(any(Speciality.class));
     }
 
     @Test
     void deleteById() {
+        //given--
+        //when--
         specialtyRepository.deleteById(1l);
-        verify(specialtyRepository).deleteById(1l);
+        //then--
+        //verify(specialtyRepository).deleteById(1l);
+        then(specialtyRepository).should().deleteById(1l);
     }
     @Test
     void deleteByIdRun2Times() {
+        //given--None
+        //When--
         specialtyRepository.deleteById(1l);
         specialtyRepository.deleteById(1l);
-        verify(specialtyRepository, times(2)).deleteById(1l);
+        //Then--
+        //verify(specialtyRepository, times(2)).deleteById(1l);
+        then(specialtyRepository).should(times(2)).deleteById(1l);
     }
     @Test
     void deleteByIdRunAtleast() {
+        //Given--None
+        //When--
         specialtyRepository.deleteById(1l);
         specialtyRepository.deleteById(1l);
-        verify(specialtyRepository, atLeast(2)).deleteById(1l);
+        //Then--
+        //verify(specialtyRepository, atLeast(2)).deleteById(1l);
+        then(specialtyRepository).should(atLeast(2)).deleteById(1l);
     }
     @Test
     void deleteByIdRunAtMost() {
+        //Given--
+        //When--
         specialtyRepository.deleteById(1l);
         specialtyRepository.deleteById(1l);
-        verify(specialtyRepository, atMost(2)).deleteById(1l);
+        //Then--
+       // verify(specialtyRepository, atMost(2)).deleteById(1l);
+        then(specialtyRepository).should(atMost(2)).deleteById(1l);
     }
 
 }
